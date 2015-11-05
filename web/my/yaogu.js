31b532f495ddbec2fea04d3030060ce383e83a41
itech001 <itech001@126.com>
2015-11-04 23:25:45 -0500

    update order

--- web/my/yaogu.js
+++ web/my/yaogu.js
@@ -6,23 +6,23 @@
     ordering: true,
     orderMulti : true,
     pageLength: 100,
-    order: [[ 2, 'desc' ], [ 3, 'desc' ], [4,'desc'], [5,'desc']],
+    order: [[ 2, 'desc' ], [5,'desc']],
     columnDefs: [
     {
       targets: [ 2 ],
-      orderData: [ 2, 3, 4, 5 ]
+      orderData: [ 2, 5 ]
     },
     {
       targets: [ 3 ],
-      orderData: [ 3, 2, 4, 5 ]  //如果第二列进行排序，有相同数据则按照第一列顺序排列
+      orderData: [ 3, 2,5 ]  //如果第二列进行排序，有相同数据则按照第一列顺序排列
     },
     {
       targets: [ 4 ],
-      orderData: [ 4, 2, 3, 5 ]
+      orderData: [ 4, 2,5 ]
     },
     {
       targets: [ 5],
-      orderData: [ 5, 2, 3, 4 ]
+      orderData: [ 5, 2,3,4 ]
     },
     {
       targets: [ 0 ],
