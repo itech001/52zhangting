155d6246fb24befb38a08a2b7fcc6b727b79823b
itech001 <itech001@126.com>
2015-11-08 08:10:57 -0500

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,17 @@
 
-python get_history.py > web/get_history.txt 2>&1  
-python gen_yaogu.py  
+#get stock data
+#python get_history.py > web/get_history.txt 2>&1
 
+#generate pages
+python gen_yaogu.py
+python gen_yaogu2.py
+
+#generate website
+cd web2/
+make publish
+cp -rf ../web/* v1/
+
+#submit
 git add .
 git commit -m "update"
-git push 
+git push
